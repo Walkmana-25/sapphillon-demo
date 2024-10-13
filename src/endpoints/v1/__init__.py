bd9f46fa91f08f0e7b468f2f143aab2bf2db9e54
Yuta Takahashi <y.highbridge04@gmail.com>
2024-10-13 13:47:30 +0000

    fix lint

--- src/endpoints/v1/__init__.py
+++ src/endpoints/v1/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from .version import router as version_router
 from .ai_search import router as ai_search_router
+from .version import router as version_router
 
 router = APIRouter()
 
