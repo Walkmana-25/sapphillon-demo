736019b2089939f745297003fc3b88c8390e3b00
Yuta Takahashi <y.highbridge04@gmail.com>
2024-10-27 03:31:36 +0000

    Refactor search component to handle missing search query parameter and add navigation bar

--- sapphillon-ui-demo/src/search.tsx
+++ sapphillon-ui-demo/src/search.tsx
@@ -1,12 +1,29 @@
 import { useSearchParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Box, Input, Flex, HStack, Icon } from '@chakra-ui/react';
+import { TbDog } from "react-icons/tb";
 
 
 function SearchView() {
-    
+
+    const [loadState, setLoadState] = useState(true);
+
     const [searchParams] = useSearchParams();
     const searchParam = searchParams.get('q');
-    
+
+    const navBar = () => {
+        return (
+            <Box bg={"gray.100"} px={"4px"}>
+                <Flex h={"10"} alignItems={"center"} justifyContent={"space-between"}>
+                    <HStack as="a" href="/" display="flex">
+                        <Icon as={TbDog} color="blue.500" boxSize={"8"} />
+                        <Box>Sapphillon Search Demo</Box>
+                    </HStack>
+                </Flex>
+            </Box>
+        )
+    }
+
     // searchParamがあるか確認する。
     // なければ、/にリダイレクトする。
     useEffect(() => {
@@ -14,14 +31,21 @@ function SearchView() {
             window.location.href = '/?err=No search query provided.';
         }
     }, [searchParam]);
-    
-    return (
-        <>
+
+    if (loadState) {
+        return (
+            <>
+                {navBar()}
+                <h1>test</h1>
+                <p>{searchParam}</p>
+            </>
+        )
+    } else {
+        return (
             <h1>test</h1>
-            <p>{searchParam}</p>
-        </>
-    )
-    
+        )
+    }
+
 }
 
 export default SearchView;
